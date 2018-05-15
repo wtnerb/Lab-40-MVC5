d8c6b8ae8ee8abac3389b39c11bc5f2f329c2517
wtnerb <wtnerb@gmail.com>
2018-05-14 22:09:32 -0700

    complete

---
+++ MvcMovie45/MvcMovie45/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie45.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "this is my <b>default</b> action";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {name}, number of times is {numTimes}.");
+        }
+    }
+}
